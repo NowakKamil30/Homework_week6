72359bccc905aa863d369e338daaaa9235cd96aa
Kamil Nowak <kamilnowakx98@gmail.com>
2020-06-04 20:11:35 +0200

    first commit

---
+++ src/main/java/pl/akademiaspringa/homeworkweek6/Homeworkweek6Application.java
@@ -0,0 +1,13 @@
+package pl.akademiaspringa.homeworkweek6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homeworkweek6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homeworkweek6Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/akademiaspringa/homeworkweek6/Homeworkweek6ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademiaspringa.homeworkweek6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homeworkweek6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
